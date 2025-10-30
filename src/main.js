6785f3c33a4b7efeef08c144a17c26eda3dc3d86
kasrajamei <kasrajamei@yahoo.com>
2025-10-30 22:02:56 +0330

    fix: directories

--- src/main.js
+++ src/main.js
@@ -1,6 +1,44 @@
-import './assets/main.css'
-
+// src/main.ts
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { fa, en } from 'vuetify/locale' 
+import '@mdi/font/css/materialdesignicons.css'
+
+import i18n from './i18n'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  locale: {
+    locale: 'en',
+    fallback: 'en',
+    messages: { fa, en },
+  },
+  rtl: {
+    fa: true,
+  },
+  theme: {
+      defaultTheme: 'light', 
+  },
+  icons: {
+      defaultSet: 'mdi',
+  }
+})
+
+app.use(pinia)
+app.use(router)
+app.use(i18n)
+app.use(vuetify)
+
+app.mount('#app')
