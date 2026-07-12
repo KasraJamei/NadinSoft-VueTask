5439e17d209337947700c213b490082f5c79f7d1
Kasra Jamei <kasrajamei@yahoo.com>
2026-07-12 18:58:10 +0330

    fix(glass-ui): add comment

--- src/components/FirstVisitModal.vue
+++ src/components/FirstVisitModal.vue
@@ -3,7 +3,7 @@ import { ref, onMounted, nextTick } from 'vue';
 import { useSettingsStore } from '@/stores/settings';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
-
+## hi
 const { t } = useI18n();
 const settingsStore = useSettingsStore();
 const router = useRouter();
